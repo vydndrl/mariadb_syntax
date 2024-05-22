48d245966a4123b9059c4b1c14886a17febda011
vydndrl <vydndrl1234@naver.com>
2024-05-22 17:40:56 +0900

    0522

--- 11.dump_practice.sh
+++ 11.dump_practice.sh
@@ -5,3 +5,22 @@ mysqldump -u root -p --default-character-set=utf8mb4 board > dumpfile.sql
 mysqldump -uroot -p board -r dumpfile.sql
 
 # dump 파일을 github에 업로드
+
+# 리눅스에서 mariadb 서버 설치
+sudo apt-get install mariadb-server
+
+# mariadb 서버 시작
+sudo systemctl start mariadb
+
+# mariadb 접속 테스트
+sudo mariadb -u root -p
+
+# git 설치
+sudo apt install git
+
+# git을 통해 repo clone
+git clone 레파지토리 주소
+
+
+sudo mysql -u root -p board < dumpfile.sql
+
---
+++ 12.user_view_procedure.sql
@@ -0,0 +1,3 @@
+-- 사용자 관리
+-- 사용자 목록 조회
+select * from mysql.user;
